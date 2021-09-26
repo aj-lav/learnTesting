ad10cd8707528064871c8531b3e614a554bf206c
aj-lav <ajeesh77krish@gmail.com>
2021-09-26 22:40:19 +0530

    initial commit

---
+++ src/main/java/com/aj/learnTesting/LearnTestingApplication.java
@@ -0,0 +1,14 @@
+package com.aj.learnTesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnTestingApplication.class, args);
+		System.out.println("Hello world");
+	}
+
+}
---
+++ src/test/java/com/aj/learnTesting/LearnTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aj.learnTesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
